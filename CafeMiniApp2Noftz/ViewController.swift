912fe9b6eb60d1d3f307eaca6bf502cdf83c1a72
Evangeline Noftz <esnoftz@gmail.com>
2024-09-17 08:07:46 -0500

    Worked on redoing UI.

--- CafeMiniApp2Noftz/ViewController.swift
+++ CafeMiniApp2Noftz/ViewController.swift
@@ -1,18 +1,70 @@
 //
 //  ViewController.swift
-//  CafeMiniApp2Noftz
+//  CafeMiniAppNoftz
 //
-//  Created by EVANGELINE NOFTZ on 9/16/24.
+//  Created by EVANGELINE NOFTZ on 9/11/24.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    /*var food: [String] = ["Cookies", "Potatoes", "Corn", "Strawberries", "Broccoli"]
+    
+    var prices: [Double] = [12.99, 3.67, 1.45, 10.45, 4.23]
+    
+    var cart: [String: Double] = [:]
+    
+    @IBOutlet weak var menuTextView: UITextView!
+    
+    
+    @IBOutlet weak var foodNameInput: UITextField!
+    
+    @IBOutlet weak var foodQuantityInput: UITextField!
+    
+    @IBOutlet weak var orderingSubmitButton: UIButton!
+    
+    
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for i in 0..<food.count{
+            menuTextView.text += "\n\(food[i]) : $\(prices[i])"
+        }
+        
+        
+        
+        //resignfirstresponder gets rid of keyboard
+        
+        
     }
+    
+    
+    @IBAction func orderingSubmitAction(_ sender: UIButton) {
+        
+//        errorLabel.text = ""
+//
+//        // "" is automatically put into order when text field is empty
+//        var foodName = foodNameInput.text!
+//        var foodPrice = foodPriceInput.text!
+//
+//        if foodName != "" || foodPrice != ""{
+//            yourCartTextView.text += "\n\(foodName) "
+//        } else {
+//            errorLabel.text = "Error!"
+//        }
+//
+//        foodNameInput.text = ""
+        
+    }*/
+    
+    
 
 
 }
